382b10bd08ba56c8596de883e271dfe891c3a83d
Minh Vu <torranceminhvu@yahoo.com>
2016-03-23 20:19:27 -0700

    first commit for hangman

---
+++ Hangman/src/hangman/Hangman.java
@@ -0,0 +1,21 @@
+/*
+ *  Program created by Minh Vu
+ * 
+ * 
+ */
+package hangman;
+
+/**
+ *
+ * @author Minh
+ */
+public class Hangman {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
